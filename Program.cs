7026806c9e0284329b0b60a2e60f6ba5d9fd3260
Estudiante Ltic <estudiante@sistemas.local>
2024-06-26 15:23:16 -0500

    Agrega mensaje linea 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ejercicio para calcular cosas
+
+int a=0;
+int b=0;
+
+Console.WriteLine(a+b);
+Console.WriteLine(a+b+1);
+Console.WriteLine("Nuevo cambio");
